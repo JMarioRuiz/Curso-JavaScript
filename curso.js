2d93e4263c55a64be1f3b58642729b97dd550a51
Mario Ruiz <josemario.bros@hotmail.com>
2024-10-14 20:45:18 -0700

    variables y constantes

--- curso.js
+++ curso.js
@@ -6,3 +6,16 @@ alert("ejemplo alerta");
 
 //para recibir datos desde el navegador
 prompt("Dame tu nombre: ");
+
+//como declarar variables
+let nombre = "Uriel";
+
+//como declarar constantes
+const PI = 3.1416;
+
+/*
+los nombres de las variables no pueden tener espacios en blanco
+los nombres de las variables no pueden iniciar con numeros
+ciertas palabras no pueden ser variables ya que son palabras reservadas de js
+*/
+
