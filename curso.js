b72a30bdaa469f0a9016ced5fd3dadc63e471893
Mario Ruiz <josemario.bros@hotmail.com>
2024-10-15 17:47:04 -0700

    Tipos de datos

--- curso.js
+++ curso.js
@@ -19,3 +19,28 @@ los nombres de las variables no pueden iniciar con numeros
 ciertas palabras no pueden ser variables ya que son palabras reservadas de js
 */
 
+let edad = 24;
+let cambio = 2.5;
+
+//si queremos realizar operaciones complejas, se puede emplear la biblioteca Math
+Math.pi;
+Math.pow(10,2);
+
+//TIPO DE DATOS
+/*
+string
+number
+boolean
+undefined
+symbol
+null
+*/
+
+//convertir un tipo de dato a otro
+parseInt();
+parseFloat();
+parseString();
+
+/*todos los objetos de js incluyen el metodo toString() para convertirlo a 
+un string
+*/
